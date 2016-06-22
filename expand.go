62bf87a0381f0ce26a5e03cbf45edd0dc6ecc3e1
jeff <jeff>
2016-06-22 01:07:14 -0700

    decompressor tests

--- expand.go
+++ expand.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"encoding/xml"
+	"archive/tar"
+	"archive/zip"
+	"compress/gzip"
+	"errors"
+	"fmt"
 	"log"
 	"os"
+	"strings"
+)
+
+const (
+	ZIP    string = ".zip"
+	TAR_GZ string = ".tar.gz"
+	GZ     string = ".gz"
+	TAR    string = ".tar"
+)
+
+var (
+	// order matters here. Must check tar.gz before gz
+	extensions = []string{ZIP, TAR_GZ, GZ, TAR}
 )
 
 func init() {
@@ -23,14 +40,89 @@ func main() {
 
 func Expand(publisher, filename string) error {
 
-	f, err := os.Open(filename)
+	reader, err := decompress(filename)
 	if err != nil {
 		return err
 	}
 
-	decoder := xml.NewDecoder(f)
+	switch reader.(type) {
+	case *tar.Reader:
 
-	err = Parse(publisher, decoder)
+	case *gzip.Reader:
 
-	return nil
+	case *zip.Reader:
+
+	default:
+		return errors.New(fmt.Sprintf("Expand got invalid type (%T)", reader))
+	}
+	err := Parse(publisher, nil)
+
+	return err
+}
+
+func decompress(filename string) (interface{}, error) {
+
+	var (
+		reader interface{}
+		err    error
+	)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ext := range extensions {
+
+		if !strings.HasSuffix(filename, ext) {
+			continue
+		}
+
+		switch ext {
+		case ZIP:
+			reader, err = unzip(file)
+		case GZ:
+			reader, err = ungzip(file)
+		case TAR:
+			reader, err = untar(file)
+		case TAR_GZ:
+			reader, err = untargz(file)
+		}
+
+		if err != nil {
+			return nil, err
+		}
+		return reader, err
+	}
+	return nil, errors.New(fmt.Sprintf("invalid extension for filename (%s)", filename))
+}
+
+func unzip(file *os.File) (*zip.Reader, error) {
+	stat, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	reader, err := zip.NewReader(file, stat.Size())
+	if err != nil {
+		return nil, err
+	}
+
+	return reader, nil
+}
+
+func ungzip(file *os.File) (*gzip.Reader, error) {
+	return gzip.NewReader(file)
+}
+
+func untar(file *os.File) (*tar.Reader, error) {
+	return tar.NewReader(file), nil
+}
+
+func untargz(file *os.File) (*tar.Reader, error) {
+	reader, err := ungzip(file)
+	if err != nil {
+		return nil, err
+	}
+	return tar.NewReader(reader), nil
 }
---
+++ expand_test.go
@@ -0,0 +1,256 @@
+package main
+
+import (
+	"archive/tar"
+	"archive/zip"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/satori/go.uuid"
+)
+
+var decompressTest = map[string]func(*testing.T) string{
+	"zip":    zipFile,
+	"gzip":   gzipFile,
+	"tar":    tarFile,
+	"tar.gz": tarGzFile,
+}
+
+func TestDecompress(t *testing.T) {
+
+	for _, v := range decompressTest {
+		filename := v(t)
+		reader, _ := decompress(filename)
+
+		switch reader.(type) {
+		case *gzip.Reader:
+			r := reader.(*gzip.Reader)
+			buf := bytes.NewBuffer(nil)
+			io.Copy(buf, r)
+			log.Println(buf.String())
+
+		case *zip.Reader:
+			r := reader.(*zip.Reader)
+			for _, f := range r.File {
+				log.Println(f)
+			}
+
+		case *tar.Reader:
+			r := reader.(*tar.Reader)
+			for {
+				h, err := r.Next()
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+				}
+				log.Println(h)
+			}
+
+		default:
+			log.Printf("%T", reader)
+		}
+	}
+
+}
+
+func tarFile(t *testing.T) string {
+	file, err := tempFile("tar")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := _tar(t)
+
+	_, err = io.Copy(file, reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return file.Name()
+}
+
+func tarGzFile(t *testing.T) string {
+
+	file, err := tempFile("tar.gz")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gw := gzip.NewWriter(file)
+	defer gw.Close()
+	tw := tar.NewWriter(gw)
+
+	var files = []struct {
+		Name, Body string
+	}{
+		{"foo.txt", "foo bar baz."},
+		{"bar.txt", "ni ni ni"},
+		{"baz.txt", "green eggs and ham"},
+	}
+	for _, file := range files {
+
+		h := &tar.Header{
+			Name:  file.Name,
+			Size:  int64(len(file.Body)),
+			Uname: os.Getenv("USER"),
+			Gname: os.Getenv("USER"),
+			Mode:  664,
+		}
+
+		err := tw.WriteHeader(h)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = tw.Write([]byte(file.Body))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+	}
+	err = tw.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return file.Name()
+}
+
+func gzipFile(t *testing.T) string {
+	file, err := tempFile("gz")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := _gz(t)
+
+	_, err = io.Copy(file, reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return file.Name()
+}
+
+func zipFile(t *testing.T) string {
+
+	file, err := tempFile("zip")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := _zip(t)
+
+	_, err = io.Copy(file, reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return file.Name()
+}
+
+func _zip(t *testing.T) io.Reader {
+
+	buf := new(bytes.Buffer)
+
+	w := zip.NewWriter(buf)
+
+	var files = []struct {
+		Name, Body string
+	}{
+		{"foo.txt", "foo bar baz."},
+		{"bar.txt", "ni ni ni"},
+		{"baz.txt", "green eggs and ham"},
+	}
+	for _, file := range files {
+		f, err := w.Create(file.Name)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = f.Write([]byte(file.Body))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	err := w.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return bytes.NewReader(buf.Bytes())
+}
+
+func _gz(t *testing.T) io.Reader {
+
+	buf := new(bytes.Buffer)
+
+	w := gzip.NewWriter(buf)
+
+	_, err := w.Write([]byte("foo bar baz"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = w.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return bytes.NewBuffer(buf.Bytes())
+}
+
+func _tar(t *testing.T) io.Reader {
+
+	buf := new(bytes.Buffer)
+
+	w := tar.NewWriter(buf)
+
+	var files = []struct {
+		Name, Body string
+	}{
+		{"foo.txt", "foo bar baz."},
+		{"bar.txt", "ni ni ni"},
+		{"baz.txt", "green eggs and ham"},
+	}
+	for _, file := range files {
+
+		h := &tar.Header{
+			Name:  file.Name,
+			Size:  int64(len(file.Body)),
+			Uname: os.Getenv("USER"),
+			Gname: os.Getenv("USER"),
+			Mode:  664,
+		}
+
+		err := w.WriteHeader(h)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = w.Write([]byte(file.Body))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+	}
+	err := w.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return bytes.NewReader(buf.Bytes())
+}
+
+func tempFile(ext string) (*os.File, error) {
+	fn := uuid.NewV4()
+	return os.Create(fmt.Sprintf("%s/%s.%s", "/tmp", fn.String(), ext))
+}
